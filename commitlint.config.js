e4c58a40c1f897f97a99ff087e8fc24b8ac453a1
hankin.dream <mm154620@163.com>
2022-09-15 11:15:08 +0800

    feat(commitlint): type enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,27 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "update",
+        "fix",
+        "perf",
+        "style",
+        "docs",
+        "test",
+        "refactor",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+        "wip",
+        "workflow",
+        "types",
+        "release",
+      ],
+    ],
+  },
+};
